32875c81aebb0307aba47fa779b93578354976bc
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-02 02:18:32 -0400

    Ran codemods to remove jQuery

--- tests/integration/components/flot-chart-test.js
+++ tests/integration/components/flot-chart-test.js
@@ -1,46 +1,53 @@
-import { test, moduleForComponent } from 'ember-qunit';
+import { find } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 import startApp from '../../helpers/start-app';
 import Ember from 'ember';
 
 var App;
 
-moduleForComponent('flot-chart', 'FlotChartComponent', {
-  setup: function () {
-    App = startApp();
-  },
-  teardown: function () {
-    Ember.run(App, 'destroy');
-  }
-});
+module('FlotChartComponent', function(hooks) {
+  setupTest(hooks);
 
-test('is a div tag', function () {
-  equal('DIV', this.$().prop('tagName'));
-});
+  hooks.beforeEach(function() {
+    this.setup = function () {
+      App = startApp();
+    };
 
-test('default plot height', function () {
-  equal(this.$().height(), 300);
-});
-
-test('plot height changes', function () {
-  var component = this.subject();
-
-  equal(this.$().height(), 300);
-
-  Ember.run(function () {
-    component.set('height', "200px");
+    this.teardown = function () {
+      Ember.run(App, 'destroy');
+    };
   });
 
-  equal(this.$().height(), 200);
-});
-
-test('plot height changes', function () {
-  var component = this.subject();
-
-  equal(this.$().height(), 300);
-
-  Ember.run(function () {
-    component.set('height', "200px");
+  test('is a div tag', function () {
+    equal('DIV', find('*').tagName);
   });
 
-  equal(this.$().height(), 200);
+  test('default plot height', function () {
+    equal(this.$().height(), 300);
+  });
+
+  test('plot height changes', function () {
+    var component = this.owner.factoryFor('component:flot-chart').create();
+
+    equal(this.$().height(), 300);
+
+    Ember.run(function () {
+      component.set('height', "200px");
+    });
+
+    equal(this.$().height(), 200);
+  });
+
+  test('plot height changes', function () {
+    var component = this.owner.factoryFor('component:flot-chart').create();
+
+    equal(this.$().height(), 300);
+
+    Ember.run(function () {
+      component.set('height', "200px");
+    });
+
+    equal(this.$().height(), 200);
+  });
 });
